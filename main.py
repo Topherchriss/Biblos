514c6f076abd56dd34a46e2e54347c9c281722e5
Topherchriss <outlawsrabut@gmail.com>
2023-11-25 15:53:41 +0300

    book class

--- main.py
+++ main.py
@@ -26,3 +26,31 @@ class User:
 
     def __str__(self):
         return f"ID: {self.user_id} Name: {self.first_name} {self.last_name} Email: {self.email} Books borrowed: {self.books_borrowed}"
+
+
+class Book:
+    def __init__(self, book_id, title, author, ISBN, availability = False):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.availability = availability
+
+    def check_out(self, user):
+        if self.availability:
+            self.availability = False
+            user.books_borrowed.append(self)
+            print(f"{user.first_name} {user.last_name} (ID: {user.user_id}) has succefully check_out {book_title}")
+        else:
+            print(f"{self.book_title} is not available for check-out!")
+
+    def return_book(self, user):
+        if self in user.books_borrowed:
+            self.availability = True
+            user.books_borrowed.remove(self)
+            print(f"{user.first_name} {user.last_name} (ID: {user.user_id}) has succefully returned {self.title}")
+        else:
+            print(f"{self.title} is not in your borrowed books!")
+
+    def __str__(self):
+        return f"Book ID: {self.book_id}, Title: {self.title}, Author: {self.author}, ISBN: {self.ISBN}, Availability: {self.availability}"
